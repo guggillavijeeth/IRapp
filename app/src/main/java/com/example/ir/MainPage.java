4c6c93e020c71c94c901773ffbb8e0be7795ef90
Vijeeth Guggilla <vijeeth.guggilla@gmail.com>
2018-10-06 16:50:10 -0500

    Preliminary ListView display done, next step is activating the edit buttons

--- app/src/main/java/com/example/ir/MainPage.java
+++ app/src/main/java/com/example/ir/MainPage.java
@@ -1,13 +1,33 @@
 package com.example.ir;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainPage extends AppCompatActivity {
+
+    private ListView listView;
+    private BetterArrayAdapter eAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        listView = (ListView) findViewById(R.id.ListUpcoming);
+        ArrayList<Event> displayedEvents = new ArrayList<Event>();
+
+        displayedEvents.add(new Event("Harris", "10/6/2018", "10:00pm"));
+        displayedEvents.add(new Event("Badminton", "10/13/2018", "7:00pm"));
+        displayedEvents.add(new Event("Gardner", "11/2/2018", "11:00pm"));
+
+        eAdapter = new BetterArrayAdapter(this, R.layout.event_text_format, displayedEvents);
+        listView.setAdapter(eAdapter);
     }
+
 }
